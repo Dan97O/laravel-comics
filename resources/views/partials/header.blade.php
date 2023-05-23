e00b4c3895224d044f9d7e8a771b26d4134f060e
Dan97O <dan97-oddo@live.it>
2023-05-23 16:02:31 +0200

    add img logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-4 d-flex align-items-center">
-                <img src="../../img/dc-logo.png" alt="">
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="">
             </div>
             <!-- /col-4 -->
             <div class="col-8 d-flex align-items-center">
                 <ul class="d-flex gap-4">
-                    <li v-for="item in menu">
-                        <a :href="item.href">ghg</a>
+                    <li>
+                        <a>ghg</a>
                     </li>
                 </ul>
             </div>
